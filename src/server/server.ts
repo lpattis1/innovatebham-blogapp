6ef695d09ba642b5b32f12d2f824d729bfb96820
lpattis1 <lpattis1@asu.edu>
2021-11-27 18:28:06 -0600

    changed from production to development.

--- src/server/server.ts
+++ src/server/server.ts
@@ -13,11 +13,5 @@ app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "../public/index.html"))
 );
 
-// const port = process.env.PORT || 3000;
-app.listen(process.env.PORT || 3000, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on port: ${port}`));
